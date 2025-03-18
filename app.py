ddcf4b8a50f8b23e79bfc5e1685601b3f6596e8e
snoopythe3 <masonkember1+github@gmail.com>
2025-03-18 19:24:38 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import scratchattach as sa
+from scratchattach import Encoding
+scratch_username = "username"
+scratch_password = "password"
+cloud = session.connect_cloud("1149116249")
+cloud.set_var("Title", 'Encoding.encode("Welcome!")')
+cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
