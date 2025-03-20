a57e69c61f7b276cada2e2b193274dfbf16019eb
snoopythe3 <masonkember1+github@gmail.com>
2025-03-20 17:38:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ scratch_password = "password"
 cloud = session.connect_cloud("1149116249")
 cloud.set_var("Title", 'Encoding.encode("Welcome!")')
 cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
+cloud = session.connect_tw_cloud("1149116249")
+cloud.set_var("Title", 'Encoding.encode("Welcome!")')
+cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
